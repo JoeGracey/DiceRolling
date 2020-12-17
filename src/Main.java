7182940e5133e068d4e9d282c108f3b1d41a3e73
Joe Gracey <joe.gracey@gmail.com>
2020-12-16 21:34:23 -0700

    settingUpDice

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,14 @@ public class Main {
         // Initialize size of dice
         Random randomObject = new Random();
         int[] dice = new int[21];
-        // Roll dice
-        // Save each roll
-        // Save each time a number is rolled
+
+        // Roll dice and save each roll
+        for (int count = 0; count < dice.length; count++) {
+            ++dice[1 + randomObject.nextInt(20)];
+        }
+
         // Print a table with number and times rolled
+        System.out.println("Number\tTimes Rolled");
         // Print the number and how many times it was rolled
     }
 }
