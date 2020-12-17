4c502e45b274de9d0a4ca0638085ddfa237c99aa
Joe Gracey <joe.gracey@gmail.com>
2020-12-16 21:15:34 -0700

    settingUpDice

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         // Initialize size of dice
+        Random randomObject = new Random();
+        int[] dice = new int[21];
         // Roll dice
         // Save each roll
         // Save each time a number is rolled
