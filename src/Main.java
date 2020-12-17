ff0e88c4590971429535bf9cad60e0b16527b92e
Joe Gracey <joe.gracey@gmail.com>
2020-12-16 21:55:23 -0700

    settingUpDice

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         // Initialize size of dice
-        Random randomObject = new Random();
-        int[] dice = new int[21];
+        Random random = new Random();
+        // 21 because its 0 - 20 with index
+        int[] diceFrequency = new int[21];
 
         // Roll dice and save each roll
-        for (int count = 0; count < dice.length; count++) {
-            ++dice[1 + randomObject.nextInt(20)];
+        for (int count = 0; count < 1000; count++) {
+            // +1 so it's 1 - 20 instead of 0 - 19
+            ++diceFrequency[1 + random.nextInt(20)];
         }
 
         // Print a table with number and times rolled
         System.out.println("Number\tTimes Rolled");
-        // Print the number and how many times it was rolled
+
+        // Print the index number and how many times it was rolled
+        for (int count = 1; count < diceFrequency.length; count++) {
+            System.out.println(count + "\t\t" + diceFrequency[count]);
+        }
     }
 }
