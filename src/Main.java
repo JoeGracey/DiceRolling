542106032f340aa7dc5d40c6a8255824be3b8fda
Joe Gracey <joe.gracey@gmail.com>
2020-12-16 21:01:34 -0700

    setting up comments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TEST");
+        // Initialize size of dice
+        // Roll dice
+        // Save each roll
+        // Save each time a number is rolled
+        // Print a table with number and times rolled
+        // Print the number and how many times it was rolled
     }
 }
